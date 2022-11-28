265746bef5a8921111d7c85eadb9da9f1344a065
duasong111 <98256947+duasong111@users.noreply.github.com>
2022-11-28 15:46:05 +0800

    Create 001-if语句的分支.cpp

---
+++ 程序流的分支/001-if语句的分支.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,n2;
+	cout << "输入:"; cin >> n;
+	cout << "输入:"; cin >> n2;
+	if (n == n2)
+		cout << n << "=="<<n2<<"\n";
+	else
+		cout << n << "!=" << n2 <<"\n";
+	
+}
